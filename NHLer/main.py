8e1a478cd3e6d1a1c793dc76c0c195ac2cadd325
max <max@ntc.local>
2022-02-14 16:18:12 +0300

    add first request

--- NHLer/main.py
+++ NHLer/main.py
@@ -1,12 +1,24 @@
+import requests
 from flask import Flask
 from requests import request
 
-app = Flask(__name__)
+def get_teams():
+    url = "https://statsapi.web.nhl.com/api/v1/teams"
+    data = requests.get(url).json()
+    return data
 
+result=get_teams()
+print(result)
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return data.text
+
+@app.route('/test')
+def test():
+    return data.text
 
 if __name__ == "__main__":
     app.run(debug=True)
