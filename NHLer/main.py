121336fe4e66a61319bc7327e852c1efa5ef131c
patissonobobr <maklysenko@gmail.com>
2022-02-20 23:16:35 +0300

    get inside games, add lil of statistic logs

--- NHLer/main.py
+++ NHLer/main.py
@@ -3,22 +3,36 @@ from flask import Flask
 from requests import request
 
 def get_teams():
-    url = "https://statsapi.web.nhl.com/api/v1/teams"
+    url = "https://statsapi.web.nhl.com/api/v1/venues/5076"
     data = requests.get(url).json()
-    return data
+    print(data['venues'][0]['id'])
+#    for key, value in data.items():
+#        print(key, "222", value)
+#        subvalue=value.json()
+#        print(subvalue["id"])
+
+    url = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2022-01-01&endDate=2022-02-02"
+    data = requests.get(url).json()
+    days=data['dates']
+
+    print(type(days))
+    for value in days:
+#       print(value['date'])
+#       print(value['games'][0]['venue']['name'])
+#        gamesaday=value['totalItems']
+        for gamesaday in value['games']:
+            print(value['date'],gamesaday['gamePk'])
+#print(value['games'])
+
+ #   print(days)
+ #   for value in days:
+ #      print(data['dates'][value]['date'])
+
+#a_dict = {1: "one", 2: "two", 3: "three"}
+#keys = a_dict.keys()
+#keys = sorted(keys)
+#for key in keys:
+#    print(key)
 
 result=get_teams()
 print(result)
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return data.text
-
-@app.route('/test')
-def test():
-    return data.text
-
-if __name__ == "__main__":
-    app.run(debug=True)
