1351e8b421e9f45671f0415194ec8395cc0748cf
Makka <maklysenko@gmail.com>
2022-02-23 01:31:10 +0300

    2 defs for 2 teams

--- NHLer/main.py
+++ NHLer/main.py
@@ -1,67 +1,51 @@
 import requests
-from flask import Flask
-from requests import request
+from datetime import datetime
 
 def get_teams():
-    url = "https://statsapi.web.nhl.com/api/v1/venues/5076"
-    data = requests.get(url).json()
-    print(data['venues'][0]['id'])
-#    for key, value in data.items():
-#        print(key, "222", value)
-#        subvalue=value.json()
-#        print(subvalue["id"])
 
-    data = requests.get("https://statsapi.web.nhl.com/api/v1/schedule?startDate=2022-01-01&endDate=2022-03-01").json()
-    days=data['dates']
-
-    print(type(days))
+    request = requests.get("https://statsapi.web.nhl.com/api/v1/schedule?startDate=2022-01-01&endDate=2022-02-01").json()
+    days = request['dates']
     for value in days:
         for gamesaday in value['games']:
             if gamesaday['venue']['name'] == "Enterprise Center":
                 print(value['date'],"||",gamesaday['gamePk'],"||",gamesaday['venue']['name'],"||",gamesaday['teams']['home']['team']['name'],"||",gamesaday['teams']['away']['team']['name'])
+                game_id = str(gamesaday['gamePk'])
+                home_players(game_id=game_id)
+                away_players(game_id=game_id)
 
-# Список игроков
-    teamdata = requests.get("https://statsapi.web.nhl.com/api/v1/game/2021020566/boxscore").json()
-
-# Гости
-    print ('-' *25)
-    print("Гости")
-    print ('-' *25)
-    away_players = teamdata['teams']['away']['players']
-    for away_player in away_players:
-        try:
-            timeOnIce = away_players[away_player]['stats']['skaterStats']['timeOnIce']
-        except:
-            timeOnIce = 0
-        fullName = away_players[away_player]['person']['fullName']
-        print(fullName, timeOnIce)
-
-# Хозяйва
+def home_players(game_id):
+    teamdata = requests.get("https://statsapi.web.nhl.com/api/v1/game/" + game_id + "/boxscore").json()
     print ('-' *25)
     print("Хозяйва")
     print ('-' *25)
-    home_players = teamdata['teams']['home']['players']
-    for home_player in home_players:
+    location = "home"
+    print(top_three_players(teamdata=teamdata, location=location))
+
+def away_players(game_id):
+    teamdata = requests.get("https://statsapi.web.nhl.com/api/v1/game/" + game_id + "/boxscore").json()
+    print ('-' *25)
+    print("Гости")
+    print ('-' *25)
+    location = "away"
+    print(top_three_players(teamdata=teamdata, location=location))
+
+def top_three_players(teamdata,location):
+    players_list = {}
+    players = teamdata['teams'][location]['players']
+    for player in players:
         try:
-            timeOnIce = home_players[home_player]['stats']['skaterStats']['timeOnIce']
+            time_on_ice = players[player]['stats']['skaterStats']['timeOnIce']
         except:
-            timeOnIce = 0
-        fullName = home_players[home_player]['person']['fullName']
-        print(fullName, timeOnIce)
+            time_on_ice = "0:0"
+        full_name = players[player]['person']['fullName']
+        time = datetime.strptime(time_on_ice, '%M:%S')
+        minutes = time.time()
+        players_list[full_name] = minutes
+    sorted_list = (sorted(players_list.items(), key=lambda x: x[1], reverse=True)[:3])
+    return(sorted_list)
 
-#    fullName = teamdata['teams']['away']['players']['ID8477949']['person']['fullName']
-#    timeOnIce = teamdata['teams']['away']['players']['ID8477949']['stats']['skaterStats']['timeOnIce']
-#    print(fullName,"||", timeOnIce)
 
 
-#https://statsapi.web.nhl.com/api/v1/game/2021020613/boxscore/teams/away(#home)/players(#list)/ID8481580 "stats": {"skate#rStats": { "timeOnIce": "15:59"
-#print(value['games'])
-
-#a_dict = {1: "one", 2: "two", 3: "three"}
-#keys = a_dict.keys()
-#keys = sorted(keys)
-#for key in keys:
-#    print(key)
 
 result=get_teams()
-print(result)
+
