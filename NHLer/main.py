b6d30ad2603eb40d0bf5ce591e266ef6212a8a08
patissonobobr <maklysenko@gmail.com>
2022-04-15 14:29:48 +0300

    resolve some warnings

--- NHLer/main.py
+++ NHLer/main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 
+
 def get_teams():
 
     request = requests.get("https://statsapi.web.nhl.com/api/v1/schedule?startDate=2022-01-01&endDate=2022-02-01").json()
@@ -16,27 +17,29 @@ def get_teams():
                 homescore = gamesaday['teams']['home']['score']
                 awayscore = gamesaday['teams']['away']['score']
                 print("-"*25)
-                print(date,"||",game_id,"||",venue,"||",hometeam,"||",homescore,"||",awayscore,"||",awayteam)
+                print(date, "||", game_id, "||", venue, "||", hometeam, "||", homescore, "||", awayscore, "||", awayteam)
                 home_players(game_id=game_id)
                 away_players(game_id=game_id)
 
+
 def home_players(game_id):
     teamdata = requests.get("https://statsapi.web.nhl.com/api/v1/game/" + game_id + "/boxscore").json()
-    print ('-' *25)
+    print('-' *25)
     print("Хозяйва")
-    print ('-' *25)
+    print('-' *25)
     location = "home"
     print(top_three_players(teamdata=teamdata, location=location))
 
 def away_players(game_id):
     teamdata = requests.get("https://statsapi.web.nhl.com/api/v1/game/" + game_id + "/boxscore").json()
-    print ('-' *25)
+    print('-' *25)
     print("Гости")
-    print ('-' *25)
+    print('-' *25)
     location = "away"
     print(top_three_players(teamdata=teamdata, location=location))
 
-def top_three_players(teamdata,location):
+
+def top_three_players(teamdata, location):
     players_list = {}
     players = teamdata['teams'][location]['players']
     for player in players:
@@ -52,7 +55,4 @@ def top_three_players(teamdata,location):
     return(sorted_list)
 
 
-
-
-result=get_teams()
-
+result = get_teams()
